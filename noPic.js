dee1ca56af203eaab3129bfe3bc0b204e7c2b78a
unknown <gilivs93@gmail.com>
2019-02-28 20:39:32 +0200

    init project

--- noPic.js
+++
@@ -1,3 +0,0 @@
-const image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png'
-
-export default image
---
+++ src/noPic.js
@@ -0,0 +1,3 @@
+const image = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/480px-No_image_available.svg.png'
+
+export default image
